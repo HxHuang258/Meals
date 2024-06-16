3b17b392d5720a287defd94e4688eed427396b21
Harry Huang <psyhh4@exmail.nottingham.ac.uk>
2024-06-16 08:29:23 +0100

    157 - Displaying Category Items onto the gridview now

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -2,17 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart';
 
 class CategoryGridItem extends StatelessWidget {
-  const CategoryGridItem(
-    {
-      super.key,
-      required this.category
-    }
-  );
+  const CategoryGridItem({super.key, required this.category});
 
   final Category category;
 
   @override
   Widget build(BuildContext context) {
-    return const Text('hello');
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [
+          category.color.withOpacity(0.9),
+          category.color.withOpacity(0.6),
+        ],
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        ),
+      ),
+      child: Text(
+        category.title,
+        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+          color: Theme.of(context).colorScheme.onSurface,
+        ),
+      ),
+    );
   }
 }
