ef2fb0083f021065ae17c23c31bb7d4c17316bdd
Harry Huang <psyhh4@exmail.nottingham.ac.uk>
2024-06-17 21:01:03 +0100

    Added navigation to meals details screen
    Used same process as before, had a slight hiccup where i wrapped the meal item card with an inkwell, where actually the inkwell was a child of the card

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -4,9 +4,10 @@ import 'package:meals/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
-  const MealItem({super.key, required this.meal});
+  const MealItem({super.key, required this.meal, required this.onSelectedMeal});
 
   final Meal meal;
+  final void Function(Meal meal) onSelectedMeal;
 
   String get complexityText {
     return meal.complexity.name[0].toUpperCase() + meal.complexity.name.substring(1);
@@ -18,58 +19,62 @@ class MealItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-        margin: const EdgeInsets.all(8),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-        clipBehavior: Clip.hardEdge,
-        elevation: 5,
-        child: InkWell(
-          onTap: () {},
-          child: Stack(
-            children: [
-              FadeInImage(
-                  placeholder: MemoryImage(kTransparentImage),
-                  image: NetworkImage(meal.imageUrl),
-                  height: 200,
-                  width: double.infinity,
-                  fit: BoxFit.cover),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                  color: Colors.black54,
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 5, horizontal: 44),
-                  child: Column(
-                    children: [
-                      Text(meal.title,
-                          maxLines: 2,
-                          textAlign: TextAlign.center,
-                          softWrap: true,
-                          style: const TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                          overflow: TextOverflow.ellipsis),
-                      const SizedBox(height: 12),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          MealItemTrait(icon: Icons.schedule, label: '${meal.duration} min'),
-                          const SizedBox(width: 12),
-                          MealItemTrait(icon: Icons.work, label: complexityText),
-                          const SizedBox(width: 12),
-                          MealItemTrait(icon: Icons.attach_money, label: affordabilityText),
-                        ],
-                      ),
-                    ],
+    
+      return Card(
+          margin: const EdgeInsets.all(8),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          clipBehavior: Clip.hardEdge,
+          elevation: 5,
+          child: InkWell(
+            onTap: () {
+              onSelectedMeal(meal);
+              },
+            child: Stack(
+              children: [
+                FadeInImage(
+                    placeholder: MemoryImage(kTransparentImage),
+                    image: NetworkImage(meal.imageUrl),
+                    height: 200,
+                    width: double.infinity,
+                    fit: BoxFit.cover),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    color: Colors.black54,
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 5, horizontal: 44),
+                    child: Column(
+                      children: [
+                        Text(meal.title,
+                            maxLines: 2,
+                            textAlign: TextAlign.center,
+                            softWrap: true,
+                            style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                            overflow: TextOverflow.ellipsis),
+                        const SizedBox(height: 12),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            MealItemTrait(icon: Icons.schedule, label: '${meal.duration} min'),
+                            const SizedBox(width: 12),
+                            MealItemTrait(icon: Icons.work, label: complexityText),
+                            const SizedBox(width: 12),
+                            MealItemTrait(icon: Icons.attach_money, label: affordabilityText),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ));
-  }
+      );
+}
 }
